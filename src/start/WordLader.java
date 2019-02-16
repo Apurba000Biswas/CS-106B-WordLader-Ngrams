8faf61ba13dd35238339b79bb7fdd0bace567470
Apurba <apurba10024@gmail.com>
2019-02-17 00:59:21 +0600

    1.3 Added Finding Valid Neighbors for the first Word

--- src/start/WordLader.java
+++ src/start/WordLader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Queue;
@@ -16,6 +17,7 @@ public class WordLader {
 	private Queue<Stack<String>> queueOfstacks;
 	private String word1;
 	private String word2;
+	private Set<String> visitedNeighbours;
 	
 	public static void main(String[] args){
 		WordLader obj = new WordLader();
@@ -23,8 +25,12 @@ public class WordLader {
 	}
 	
 	private void run(){
+		System.out.println("Welcome to CS 106B Word Ladder.\n"
+				+ "Please give me two English words, and I will change the\n"
+				+ "first into the second by changing one letter at a time.\n\n");
 		makeDictionary();
 		takeInputFromUser();
+		findWordLader();
 	}
 	
 	private void makeDictionary(){
@@ -38,7 +44,6 @@ public class WordLader {
 					dictionary.add(word);
 				}
 			}
-			System.out.println("Done");
 			scanner.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,11 +52,11 @@ public class WordLader {
 	
 	private void takeInputFromUser(){
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter First Word:  ");
-		word1 = sc.nextLine().trim();
+		System.out.print("Word #1 :  ");
+		word1 = sc.nextLine().trim().toLowerCase();
 		while(true){
-			System.out.print("Enter Second Word:  ");
-			word2 = sc.nextLine().trim();
+			System.out.print("Word #2 :  ");
+			word2 = sc.nextLine().trim().toLowerCase();
 			if(isValidInput(word1.length(), word2)){
 				break;
 			}else{
@@ -64,5 +69,47 @@ public class WordLader {
 	
 	private boolean isValidInput(int length, String word2){
 		return word2.length() == length;
+	}
+	
+	private void findWordLader(){
+		//create an empty Queue of stacks
+		queueOfstacks = new LinkedList<Stack<String>>();
+		//Create/add a stack containing {w1} to the queue.
+		Stack<String> word1Stack = new Stack<>();
+		word1Stack.add(word1);
+		queueOfstacks.add(word1Stack);
+		while(queueOfstacks.size() > 0){
+			//Dequeue the partial-ladder stack from the front of the queue.
+			Stack<String> wordStack = queueOfstacks.remove();
+			// now get top word from the stack
+			String topWord = wordStack.pop();
+			findNeighbours(topWord);
+		}
+	}
+	
+	private void findNeighbours(String topWord){
+		String word = topWord;
+		for(int i=0; i<word.length(); i++){
+			for(char alphabet = 'a'; alphabet<='z'; alphabet++){
+				String newBuiltNeighbour = getPossibleNeighbour(i, alphabet, word);
+				if(dictionary.contains(newBuiltNeighbour)){
+					// ok its valid word
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+	
+	private String getPossibleNeighbour(int index, char alpha, String original){
+		String newStr = "";
+		for(int i=0; i<original.length(); i++){
+			char c = original.charAt(i);
+			if(i == index){
+				c = alpha;
+			}
+			newStr += c;
+		}
+		return newStr;
 	}
 }
