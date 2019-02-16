8abeb887f88dfb638cdf6e985a652fc770028ca9
Apurba <apurba10024@gmail.com>
2019-02-16 17:54:26 +0600

    1.1 Added Dictionry building from Text File

--- src/start/WordLader.java
+++ src/start/WordLader.java
@@ -1,5 +1,41 @@
 package start;
 
-public class WordLader {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class WordLader {
+	
+	private static final String FILE_NAME = "D:/Project/CS 106B WordLader Ngrams/dictionary.txt";
+	private Set<String> dictionary;
+	
+	public static void main(String[] args){
+		WordLader obj = new WordLader();
+		obj.run();
+	}
+	
+	private void run(){
+		makeDictionary();
+	}
+	
+	private void makeDictionary(){
+		dictionary = new HashSet<String>();
+		Path path = Paths.get(FILE_NAME);
+		try {
+			Scanner scanner = new Scanner(path);
+			while(scanner.hasNext()){
+				String word = scanner.next();
+				if(!dictionary.contains(word)){
+					dictionary.add(word);
+				}
+			}
+			System.out.println("Done");
+			scanner.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
