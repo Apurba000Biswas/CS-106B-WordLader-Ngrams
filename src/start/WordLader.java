37f5e4ddeaa6d0a6239877712e00b344921a657e
Apurba <apurba10024@gmail.com>
2019-02-16 22:03:19 +0600

    1.2 Added Input taking from user

--- src/start/WordLader.java
+++ src/start/WordLader.java
@@ -6,11 +6,16 @@ import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Queue;
+import java.util.Stack;
 
 public class WordLader {
 	
 	private static final String FILE_NAME = "D:/Project/CS 106B WordLader Ngrams/dictionary.txt";
 	private Set<String> dictionary;
+	private Queue<Stack<String>> queueOfstacks;
+	private String word1;
+	private String word2;
 	
 	public static void main(String[] args){
 		WordLader obj = new WordLader();
@@ -19,6 +24,7 @@ public class WordLader {
 	
 	private void run(){
 		makeDictionary();
+		takeInputFromUser();
 	}
 	
 	private void makeDictionary(){
@@ -37,5 +43,26 @@ public class WordLader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void takeInputFromUser(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First Word:  ");
+		word1 = sc.nextLine().trim();
+		while(true){
+			System.out.print("Enter Second Word:  ");
+			word2 = sc.nextLine().trim();
+			if(isValidInput(word1.length(), word2)){
+				break;
+			}else{
+				System.out.println("Second Word should match the first words length");
+			}
+		}
+		
+		sc.close();
+	}
+	
+	private boolean isValidInput(int length, String word2){
+		return word2.length() == length;
 	}
 }
